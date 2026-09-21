a229c8fb196844df2ead97e3703bd55b43a09516
codewithdhruba01 <patidhrubaraj@gmail.com>
2026-09-21 23:35:44 +0530

    feat: add server entry point to initialize and start application

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,6 @@ import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT as number, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
